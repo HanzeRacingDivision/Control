854a4b31a2f37e7a1b2df1f7fe2b195cba2d1bd8
BlueJ1 <l.august@posteo.net>
2023-03-11 15:50:30 +0100

    Added subscriber and some other changes and files added

---
+++ log/mainLog.py
@@ -0,0 +1,24 @@
+## some quick'n'dirty code for logging the main loop (ARCv0)
+
+import datetime # used to auto-generate filenames
+try:
+    from basiclogger import basicLogger
+except:
+    from log.basiclogger import basicLogger
+
+class mapLoggerClass(basicLogger):
+    def __init__(self, filename=None):
+        if(filename is None):
+            filename = "mainLog_" + datetime.datetime.now().strftime("%Y-%m-%d_%H;%M;%S")
+        basicLogger.__init__(self, filename) # auto-infer columnNames!
+    
+    def logMap(self, mapToUse):
+        dictToWrite = {'timestamp' : mapToUse.clock(),
+                       'carPosX' : mapToUse.car.position[0],
+                       'carPosY' : mapToUse.car.position[1],
+                       'carOrient' : mapToUse.car.angle,
+                       'velocity' : mapToUse.car.velocity,
+                       'steering' : mapToUse.car.steering,
+                       'desired_velocity' : mapToUse.car.desired_velocity,
+                       'desired_steering' : mapToUse.car.desired_steering}
+        self._writeLineFromDict(dictToWrite)
